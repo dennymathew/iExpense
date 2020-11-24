10bc6bfd0eca53b2d439fa88a3a49806227fde12
Denny Mathew <devdenmathew@gmail.com>
2020-11-24 23:12:02 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Denny Mathew on 24/11/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
